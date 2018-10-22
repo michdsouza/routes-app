38b9b51998eb1257f889e9cba250650d0781dcf2
michdsouza <michd2005@gmail.com>
2018-10-22 10:51:27 -0300

    Modified some css

--- src/components/Position.js
+++ src/components/Position.js
@@ -8,18 +8,18 @@ export default class Position extends Component {
 
   render() {
     const position = this.props.data.find(p => p.id == this.props.match.params.positionId)
-    let positionData = <div className="left">
+    let positionData = <div>
       <h3>Position not found</h3>
     </div>
 
     if (position) {
-      positionData = <div className="left">
-        <h3>Position Details</h3>
-        <p>Id: {position.id}</p>
-        <p>Name: {position.name}</p>
-        <p>Order: {position.order}</p>
-        <img alt="yum" src={position.image} />
-      </div>
+      positionData = <div>
+          <h3>Position Details</h3>
+          <img alt="yum" src={position.image} />
+          <p><b>Id:</b> {position.id}</p>
+          <p><b>Name:</b> {position.name}</p>
+          <p><b>Order:</b> {position.order}</p>
+        </div>;
     }
 
     return <div>{positionData}</div>

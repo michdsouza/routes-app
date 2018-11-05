7c0eafd6cebb7b2d396d819c463276397a8d59ff
michdsouza <michd2005@gmail.com>
2018-11-05 13:46:12 -0200

    Added sources to images

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import "./Position.css"
+import React, { Component } from 'react'
+import './Position.css'
 
 const positionNotFound = <div>
   <h3>Position not found</h3>
@@ -8,13 +8,40 @@ const positionNotFound = <div>
 const positionFound = position => (
   <div>
     <h3>{position.name}</h3>
-    <img alt="yum" src={position.image} />
+    <canvas
+      ref="canvas"
+      width={position.image_width}
+      height={position.image_height}
+    />
+    <img ref="image" alt="yum" src={position.image} className="hidden" />
   </div>
 )
 
 export default class Position extends Component {
   constructor(props) {
     super(props)
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    const img = this.refs.image
+    const position = this.props.positions.find(p => p.id == this.props.match.params.positionId)
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0)
+      ctx.fillStyle = 'red'
+      ctx.font = "20px Helvetica"
+      position.sources.map((source) =>
+        this.drawSource(source, ctx)
+      )
+    }
+  }
+
+  drawSource(source, ctx) {
+    ctx.fillRect(source.x, source.y, 5, 5)
+    // TODO: Remove hardcoded +8 and +5
+    ctx.fillText(source.name, source.x + 8, source.y + 5)
   }
 
   render() {
--- src/data/routesData.js
+++ src/data/routesData.js
@@ -1,35 +1,38 @@
 const routesData = {
   positions: {
-    'position-1': {
-      id: 'position-1',
-      name: 'Position 1',
-      image: '/shark1.jpg',
+    "position-1": {
+      id: "position-1",
+      name: "Position 1",
+      image: "/shark1.jpg",
       image_width: 640,
       image_height: 330,
-      sources: [{ x: 10, y: 10 }]
+      sources: [
+        { x: 230, y: 20, name: "Fin" },
+        { x: 90, y: 120, name: "Tail" },
+      ]
     },
-    'position-2': {
-      id: 'position-2',
-      name: 'Position 2',
-      image: '/shark2.jpg',
+    "position-2": {
+      id: "position-2",
+      name: "Position 2",
+      image: "/shark2.jpg",
       image_width: 800,
       image_height: 533,
-      sources: [{ x: 10, y: 10 }]
+      sources: [{ x: 620, y: 350, name: "Fin" }]
     },
-    'position-3': {
-      id: 'position-3',
-      name: 'Position 3',
-      image: '/shark3.jpg',
+    "position-3": {
+      id: "position-3",
+      name: "Position 3",
+      image: "/shark3.jpg",
       image_width: 640,
       image_height: 320,
-      sources: [{ x: 10, y: 10 }]
+      sources: [{ x: 600, y: 280, name: "Fin" }]
     }
   },
   routes: [
     {
-      id: 'route-a',
-      title: 'Route A',
-      positionIds: ['position-1', 'position-2', 'position-3']
+      id: "route-a",
+      title: "Route A",
+      positionIds: ["position-1", "position-2", "position-3"]
     }
   ]
 };
